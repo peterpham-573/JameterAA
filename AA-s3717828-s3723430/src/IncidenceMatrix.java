11a584f688aa1229a70f689e124947ec15e4ebc7
Jamal Ibaida <s3723430@student.rmit.edu.au>
2019-04-16 13:36:31 +1000

    pretty much nothing

--- AA-s3717828-s3723430/src/IncidenceMatrix.java
+++ AA-s3717828-s3723430/src/IncidenceMatrix.java
@@ -1,34 +1,47 @@
 import java.io.*;
 import java.util.*;
-
+import com.sun.javafx.geom.Edge;
 
 /**
  * Incident matrix implementation for the AssociationGraph interface.
  *
- * Your task is to complete the implementation of this class.  You may add methods, but ensure your modified class compiles and runs.
+ * Your task is to complete the implementation of this class. You may add
+ * methods, but ensure your modified class compiles and runs.
  *
  * @author Jeffrey Chan, 2019.
  */
-public class IncidenceMatrix extends AbstractAssocGraph
-{
+public class IncidenceMatrix extends AbstractAssocGraph {
+
+	private int numOfVertex = 0;
+	private Map<String,Integer> VertexK = new HashMap<String,Integer>();
+	private Map<String,Integer> EdgeK = new HashMap<String,Integer>();
+	private Edge matrix[][];
 
 	/**
 	 * Contructs empty graph.
 	 */
-    public IncidenceMatrix() {
-    	// Implement me!
-    } // end of IncidentMatrix()
+	public IncidenceMatrix() {
 
+	} // end of IncidentMatrix()
 
-    public void addVertex(String vertLabel) {
-        // Implement me!
-    } // end of addVertex()
+	public void addVertex(String vertLabel) {
+		
+		VertexK.put(vertLabel, numOfVertex);
+		EdgeK.put(vertLabel, numOfVertex);
+		
+		if(numOfVertex != 0){
 
+		
+		}
+		
+		
+	} // end of addVertex()
 
-    public void addEdge(String srcLabel, String tarLabel, int weight) {
-        // Implement me!
-    } // end of addEdge()
-
+	public void addEdge(String srcLabel, String tarLabel, int weight) {
+		
+		
+		
+	} // end of addEdge()
 
 	public int getEdgeWeight(String srcLabel, String tarLabel) {
 		// Implement me!
@@ -37,42 +50,36 @@ public class IncidenceMatrix extends AbstractAssocGraph
 		return EDGE_NOT_EXIST;
 	} // end of existEdge()
 
-
 	public void updateWeightEdge(String srcLabel, String tarLabel, int weight) {
-        // Implement me!
-    } // end of updateWeightEdge()
+		// Implement me!
+	} // end of updateWeightEdge()
 
-
-    public void removeVertex(String vertLabel) {
-        // Implement me!
-    } // end of removeVertex()
-
+	public void removeVertex(String vertLabel) {
+		// Implement me!
+	} // end of removeVertex()
 
 	public List<MyPair> inNearestNeighbours(int k, String vertLabel) {
-        List<MyPair> neighbours = new ArrayList<MyPair>();
+		List<MyPair> neighbours = new ArrayList<MyPair>();
 
-        // Implement me!
+		// Implement me!
 
-        return neighbours;
-    } // end of inNearestNeighbours()
+		return neighbours;
+	} // end of inNearestNeighbours()
 
+	public List<MyPair> outNearestNeighbours(int k, String vertLabel) {
+		List<MyPair> neighbours = new ArrayList<MyPair>();
 
-    public List<MyPair> outNearestNeighbours(int k, String vertLabel) {
-        List<MyPair> neighbours = new ArrayList<MyPair>();
+		// Implement me!
 
-        // Implement me!
+		return neighbours;
+	} // end of outNearestNeighbours()
 
-        return neighbours;
-    } // end of outNearestNeighbours()
+	public void printVertices(PrintWriter os) {
+		// Implement me!
+	} // end of printVertices()
 
-
-    public void printVertices(PrintWriter os) {
-        // Implement me!
-    } // end of printVertices()
-
-
-    public void printEdges(PrintWriter os) {
-        // Implement me!
-    } // end of printEdges()
+	public void printEdges(PrintWriter os) {
+		// Implement me!
+	} // end of printEdges()
 
 } // end of class IncidenceMatrix
